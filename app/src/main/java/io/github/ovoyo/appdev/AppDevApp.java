ece3b47e002a6157d6d00b1594209abf73e6734c
OVOYO <zhangyn723@foxmail.com>
2018-04-10 18:43:42 +0800

    bmbo-sdk

--- app/src/main/java/io/github/ovoyo/appdev/AppDevApp.java
+++ app/src/main/java/io/github/ovoyo/appdev/AppDevApp.java
@@ -5,6 +5,7 @@ import android.app.Application;
 
 import javax.inject.Inject;
 
+import cn.bmob.v3.Bmob;
 import dagger.android.AndroidInjector;
 import dagger.android.DispatchingAndroidInjector;
 import dagger.android.HasActivityInjector;
@@ -12,6 +13,8 @@ import io.github.ovoyo.appdev.di.AppInjector;
 
 
 public class AppDevApp extends Application implements HasActivityInjector{
+
+    private static final String BMOB_APP_KEY = "bf43dd19d5df4d553b420a56319c3860";
 
     @Inject
     DispatchingAndroidInjector<Activity> mDispatchingAndroidInjector;
@@ -21,6 +24,8 @@ public class AppDevApp extends Application implements HasActivityInjector{
         super.onCreate();
 
         AppInjector.init(this);
+
+        Bmob.initialize(this,BMOB_APP_KEY);
     }
 
     @Override
